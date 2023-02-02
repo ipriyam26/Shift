fb1e46cf41f6997e982848c438033667608c56ed
codewithjaspreet <jaspreetsodhi02@gmail.com>
2023-02-03 01:32:46 +0530

    graph-thoda

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:money_goes_brr/widgets/graph-3.dart';
 
 import 'authentication/view/login.dart';
 import 'authentication/view/sign_in.dart';
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
       builder: (context, a) {
         return  GetMaterialApp(
           debugShowCheckedModeBanner: false,
-          home: SignIn(),
+          home:LineChartSample2(),
         );
       },
     );
