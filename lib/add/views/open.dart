216e47a77fd0b40d16cfbee0068b2fbaa05051ca
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-02-05 03:38:23 +0530

    made add post

--- lib/add/views/open.dart
+++ lib/add/views/open.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:money_goes_brr/add/controller/add.dart';
+import 'package:money_goes_brr/add/views/post.dart';
+import 'package:money_goes_brr/user/controller/user.dart';
 
 class Open extends StatelessWidget {
   Open({super.key});
-
+  UserController userController = Get.put(UserController());
   AddController addController = Get.put(AddController());
 
 // show a modal bottom sheet when the user taps the button and give option to pick image or video
@@ -21,17 +23,28 @@ class Open extends StatelessWidget {
                   title: const Text('Image'),
                   onTap: () async {
                     var f = await addController.pickimagefromgallery();
-                    print(f?.path);
+                    addController.pickedFile.value = f;
+// go to post screen
+                    addController.pickedFile.refresh();
+                    Get.to(() => PostScreen());
                   },
                 ),
                 ListTile(
                   leading: const Icon(Icons.video_collection),
                   title: const Text('Video'),
                   onTap: () async {
+                    addController.isVideoo.value = true;
+                    addController.isVideoo.refresh();
                     var f = await addController.pickvideofromgallery();
-                    print(f?.path);
-                    var d = await addController.uploadVideo(f!);
-                    print(d);
+                    addController.pickedFile.value = f;
+                    var thumbnail = await addController.getThumbnail(f!);
+                    addController.thumbnail.value = thumbnail;
+
+                    addController.pickedFile.refresh();
+                    addController.thumbnail.refresh();
+
+                    print(thumbnail.path);
+                    Get.to(() => PostScreen());
                   },
                 ),
               ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:money_goes_brr/add/views/open.dart';
 import 'package:money_goes_brr/add/views/post.dart';
-
-
-
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +21,7 @@ class MyApp extends StatelessWidget {
       builder: (context, a) {
         return GetMaterialApp(
           debugShowCheckedModeBanner: false,
-          home: PostScreen(),
+          home: Open(),
         );
       },
     );
